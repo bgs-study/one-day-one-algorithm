6b9498315cc845ec03e1d1944f7aa6e0288c7ef6
Park Ji Hye <qkrwlgp235@gmail.com>
2023-02-06 21:46:23 +0900

    타켓넘버/ Lv2 / X

---
+++ ParkJiHye/JS/programmers/DFSBFS/타켓넘버.js
@@ -0,0 +1,18 @@
+function solution(numbers, target) {
+    let answer = 0;
+    DFS(0, 0);
+
+    function DFS(level, sum) {
+        if (level === numbers.length) {
+            if (sum === target) {
+                answer += 1;
+            }
+            return;
+        }
+        DFS(level + 1, sum + numbers[level]);
+        DFS(level + 1, sum - numbers[level]);
+	};
+    return answer;
+}
+
+solution(numbers,target);
